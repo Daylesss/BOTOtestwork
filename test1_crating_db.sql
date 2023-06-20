bebf00a3c88b5abe0f1bb50569d7f1725cbe30b8
Daylesss <137228350+Daylesss@users.noreply.github.com>
2023-06-21 05:30:19 +0700

    Update test1_crating_db.sql

--- test1_crating_db.sql
+++ test1_crating_db.sql
@@ -1,1 +1,19 @@
+CREATE TABLE empl_post_grades
+(
+employee varchar,
+post varchar,
+c1 int,
+c2 int,
+c3 int,
+c4 int
+);
+
+INSERT INTO empl_post_grades
+VALUES
+(
+'e1' , 'p1', 2, NULL, 3, 2
+),
+('e2' , 'p3', 3, 2, 1, 1),
+('e3' , 'p2', NULL,3,1, NULL),
+('e4' , 'p4', 2, 3, NULL, NULL);
 
