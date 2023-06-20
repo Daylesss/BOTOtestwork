4dd38e6e2ed95f2afcad8b514a05c7eef89099be
Daylesss <137228350+Daylesss@users.noreply.github.com>
2023-06-21 05:32:39 +0700

    Update test1_crating_db.sql

--- test1_crating_db.sql
+++ test1_crating_db.sql
@@ -17,3 +17,54 @@ VALUES
 ('e3' , 'p2', NULL,3,1, NULL),
 ('e4' , 'p4', 2, 3, NULL, NULL);
 
+CREATE TABLE course_post
+(
+post varchar,
+course1 bool,
+course2 bool,
+course3 bool,
+course4 bool	
+);
+
+INSERT INTO course_post
+VALUES
+(
+'p1', TRUE, FALSE, TRUE, FALSE
+),
+('p2', FALSE, TRUE, TRUE, TRUE),
+('p3', FALSE, TRUE, TRUE, FALSE),
+('p4', TRUE, TRUE, TRUE, TRUE);
+
+
+CREATE TABLE c_course
+(
+course varchar,
+c1 bool,
+c2 bool,
+c3 bool,
+c4 bool
+);
+
+INSERT INTO c_course
+VALUES
+('course1', TRUE, NULL, NULL, NULL),
+('course2', NULL, NULL, TRUE, NULL),
+('course3', NULL, NULL, NULL, TRUE),
+('course4', NULL, TRUE, NULL, NULL);
+
+CREATE TABLE c_post
+(
+post varchar,
+c1 int,
+c2 int,
+c3 int,
+c4 int
+);
+
+
+INSERT INTO c_post
+VALUES
+('p1', 3, NULL, 1, 2),
+('p2', NULL, 1,3, NULL),
+('p3', 3,3,3,1),
+('p4', 2,2, NULL, NULL);
