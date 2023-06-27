44af9d21504f2d6e978bd43836ffdb80768a4e03
Dayless <137228350+Daylesss@users.noreply.github.com>
2023-06-27 13:13:26 +0700

    new test4

--- list_of_words_implemetation.py
+++ list_of_words_implemetation.py
@@ -2,43 +2,73 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from pymorphy2 import MorphAnalyzer
-from nltk.util import ngrams
-
-# обработка предложения и проверка по словарю
-
-with open('russian_words.txt', 'r', encoding='utf-8') as list_ru_words:  #создание словаря русских слов файл russian words есть в BOTOtestwork.
+import emoji
+import pymorphy3
 
 
-    russian =list_ru_words.readlines()
-russian_words=[]
-for word in russian:
-   russian_words.append(word.rstrip('\n'))
+# Функция для определения, есть ли хотя бы один токен из русских букв в предложении.
+# Исключает предложения на других языках, но допускает предложения типа: 'Люблю Linking park'
 
-# токенизация и лемматизация текста для последующей обработки и поска по словарю.
+def is_one_russian_word(tokens: list, threeshold_russian_words: float = 0) -> bool:
+    count=0
+    for word in tokens:
+        if all(ord(char) >= 1040 and ord(char) <= 1103 for char in word):
+            if threeshold_russian_words==0: # при default=0 значении возвращает True
+                return True                 # если есть хотя бы один токен из букв русского алфавита.
+            count+=1
+    # threeshold_russian_words отвечает за минимальный процент содержания токенов из русских букв. 
+    return count/len(tokens)>=threeshold_russian_words
 
-definable=input('Введиите текст для определения:')
 
-definable=definable.lower()
+# основная функция
+# сравнивает суммарное значение score из MorphAnalyzer для лемматизации слова с пороговым.
+# threshold - пороговое значение. (настраиваемо)
 
-definable = re.sub(r'\W+', ' ', definable)
-definable = re.sub(r'\d+', '', definable)
+def is_meaningfull(user_text: str, threshold: float =0.834, threeshold_russian_words: float = 0, remove_stopwords: bool = False) ->bool:
 
-words=word_tokenize(definable, language='russian')
+    # проверка на эмодзи. Пропускает сообщения только из эмодзи.
 
-stop_words= set(stopwords.words('russian'))
-words = [word for word in words if word not in stop_words]
+    if emoji.is_emoji(user_text) and len(user_text)<4:
+        return True
+    
+    # len(user_text)<4 исключает спам эмодзи.
 
-morph = MorphAnalyzer()
-lemmas = [morph.parse(word)[0].normal_form for word in words]
+    elif emoji.is_emoji(user_text) and len(user_text)>=4:
+        return False
+    
+    # обработка предложения. Исключение всех 'небуквенных' значений.
+    user_text= user_text.lower()
+    user_text = re.sub(r'\W+', ' ', user_text)
+    user_text = re.sub(r'\d+', '', user_text)
 
-# проверка наличия слов в списке.
-# для того, чтобы текмт можно было бы считаь осмысленны в нём должно быть хотя бы одно слово русского языка.
+    # токенизация
+    tokens = words=word_tokenize(user_text, language='russian')
 
-count=0
-for lemma in lemmas:
-    if  lemma in russian_words: count+=1 break
+    if is_one_russian_word(tokens, threeshold_russian_words): # проверка на содержание токенов из русских букв
 
-if count>0: print(' в тексте есть слова русского языка')
-else: 'возможно слов русского языка в тексте нет, либо в нём содержатся неологизмы и сленговые выражения'
+        morph= pymorphy3.MorphAnalyzer()
+
+    # удаление стоп-слов если указано remove_stopwords = True
+    # Удаление стоп-слов влияет на score.
+        if remove_stopwords:
+            stop_words= set(stopwords.words('russian'))
+            tokens= [token for token in tokens if token not in stop_words]
+
+        # получение score для каждого токена и сравнение.
+
+        score=0
+
+        for word in tokens:
+            p= morph.parse(word)
+            score += p[0].score   # суммарный score
+        if score>len(tokens)*threshold:
+            return True   #если score больше threshold, предложение считается осмысленным.
+        else: return False  # в противном случае False
+
+    else:
+        return False  # возвращает False если процент токенов из русских букв ниже указанного.
+
+text = 'Было челленджево, но суперски. респект'
+
+print(is_meaningfull(text))
 
